35575803ffaae48afc390e30199b03fe9170e827
Sergey Kharlov <l1nks@inbox.ru>
2022-11-30 00:12:04 +0500

    setup connection between server/client

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,10 +1,17 @@
 import { FC } from "react"
+import axios from "axios";
 
 const App: FC = () => {
+    const handleClickButton = async () => {
+        const response = await axios.get('http://localhost:3001')
+        console.log('test', response)
+    };
+
     return (
         <div>
             {" "}
             <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+            <button onClick={handleClickButton}>Click</button>
         </div>
     )
 }
